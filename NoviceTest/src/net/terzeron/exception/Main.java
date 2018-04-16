0314bc763fdabb79ecf7de7b8ca6602d330b758d
Young-il Cho <youngilcho@nhnent.com>
2018-04-16 19:28:59 +0900

    remove a project

--- NoviceTest/src/net/terzeron/exception/Main.java
+++
@@ -1,8 +0,0 @@
-package net.terzeron.exception;
-
-/**
- * Created by terzeron on 2016. 10. 19..
- */
-public abstract class Main {
-    void toast(int a, int b) throws InvalidNumberException, NotANumberException {}
-}
--- NoviceTest/src/net/terzeron/exception/NotANumberException.java
+++
@@ -1,7 +0,0 @@
-package net.terzeron.exception;
-
-/**
- * Created by terzeron on 2016. 10. 19..
- */
-public class NotANumberException extends NumberException {
-}
